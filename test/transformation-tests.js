f1c1408b2b7ed474aa2b769fa721057f8b0ac728
meenahoda <meena.hoda@wmfs.net>
2021-10-07 13:22:57 +0100

    test: add some further tests and example

--- test/transformation-tests.js
+++ test/transformation-tests.js
@@ -5,11 +5,10 @@
 const processor = require('./../lib/')
 const chai = require('chai')
 const expect = chai.expect
-const examples = require('@wmfs/cardscript-examples')
 
 const tests = [
   {
-    cardscript: examples.complex,
+    cardscript: require('./fixtures/example-1.json'),
     input: {
       name: 'Homer Simspon',
       phoneNumber: '07123456789',
@@ -39,43 +38,38 @@ const tests = [
     }
   },
   {
-    cardscript: {
-      body: [
-        {
-          id: 'component1',
-          type: 'Input.Text'
-        },
-        {
-          showWhen: 'component1 === \'RED\'',
-          type: 'Container',
-          items: [
-            {
-              id: 'component2',
-              type: 'Input.Text'
-            },
-            {
-              showWhen: 'component2 === \'BLUE\'',
-              id: 'component3',
-              type: 'Input.Text'
-            }
-          ]
-        },
-        {
-          type: 'Container',
-          items: [
-            {
-              id: 'component4',
-              type: 'Input.Text'
-            },
-            {
-              showWhen: 'component4 === \'YELLOW\'',
-              id: 'component5',
-              type: 'Input.Text'
-            }
-          ]
-        }
-      ]
+    cardscript: require('./fixtures/example-1.json'),
+    input: {
+      name: 'Homer Simspon',
+      phoneNumber: '07123456789',
+      dietaryReq: ['GLUTEN_FREE', 'OTHER'],
+      dietaryReqOther: 'Other...',
+      size: 'M',
+      savouryOrSweet: 'SWEET',
+      base: 'TOMATO',
+      hot: true,
+      howHot: 3,
+      toppings: ['JALAPENOS', 'PINEAPPLE'],
+      primaryFlavour: 'WHITE_CHOC',
+      secondaryFlavour: 'DARK_CHOC',
+      sprinkles: false,
+      deliveryOrCollection: 'DELIVER'
     },
+    output: {
+      name: 'Homer Simspon',
+      phoneNumber: '07123456789',
+      dietaryReq: ['GLUTEN_FREE', 'OTHER'],
+      dietaryReqOther: 'Other...',
+      size: 'M',
+      savouryOrSweet: 'SWEET',
+      primaryFlavour: 'WHITE_CHOC',
+      secondaryFlavour: 'DARK_CHOC',
+      sprinkles: false,
+      deliveryOrCollection: 'DELIVER'
+    }
+  },
+  {
+    cardscript: require('./fixtures/example-2.json'),
     input: {
       component1: 'RED',
       component3: 'PINK',
@@ -85,6 +79,61 @@ const tests = [
     output: {
       component1: 'RED',
       component4: 'ORANGE'
+    }
+  },
+  {
+    cardscript: require('./fixtures/example-3.json'),
+    input: {
+      telNum: '07123456789',
+      name: 'Marge Simpson',
+      usesChipPanOrDeepFatFryer: 'NO',
+      electricBlanketsUsed: 'YES',
+      hearingImpairmentInHousehold: 'NO',
+      hearingImpairmentEquipmentRequired: 'YES',
+      numberOfSmokeAlarms: 3,
+      numberOfFloors: 2,
+      smokeAlarmsOnEachStorey: 'YES',
+      numberOfFloorsWithoutSmokeAlarms: 1,
+      ableToTestNow: 'YES_WORKING',
+      numberOfAlarmsFailedTesting: 1
+    },
+    output: {
+      telNum: '07123456789',
+      name: 'Marge Simpson',
+      usesChipPanOrDeepFatFryer: 'NO',
+      electricBlanketsUsed: 'YES',
+      hearingImpairmentInHousehold: 'NO',
+      numberOfSmokeAlarms: 3,
+      smokeAlarmsOnEachStorey: 'YES',
+      ableToTestNow: 'YES_WORKING',
+      numberOfAlarmsFailedTesting: 1
+    }
+  },
+  {
+    cardscript: require('./fixtures/example-3.json'),
+    input: {
+      telNum: '07123456789',
+      name: 'Marge Simpson',
+      usesChipPanOrDeepFatFryer: 'NO',
+      electricBlanketsUsed: 'YES',
+      hearingImpairmentInHousehold: 'YES',
+      hearingImpairmentEquipmentRequired: 'NO',
+      numberOfSmokeAlarms: 0,
+      numberOfFloors: 2,
+      smokeAlarmsOnEachStorey: 'YES',
+      numberOfFloorsWithoutSmokeAlarms: 1,
+      ableToTestNow: 'YES_WORKING',
+      numberOfAlarmsFailedTesting: 1
+    },
+    output: {
+      telNum: '07123456789',
+      name: 'Marge Simpson',
+      usesChipPanOrDeepFatFryer: 'NO',
+      electricBlanketsUsed: 'YES',
+      hearingImpairmentInHousehold: 'YES',
+      hearingImpairmentEquipmentRequired: 'NO',
+      numberOfSmokeAlarms: 0,
+      numberOfFloors: 2
     }
   }
 ]
