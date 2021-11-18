53b319b748ff5b94ad3ae38fff5ac6b5a84a03a3
meenahoda <meena.hoda@wmfs.net>
2021-11-18 13:30:12 +0000

    fix: use default values rather than setting to null

--- test/transformation-tests.js
+++ test/transformation-tests.js
@@ -2,6 +2,7 @@
 
 'use strict'
 
+const extractDefaults = require('@wmfs/cardscript-extract-defaults')
 const processor = require('./../lib/')
 const chai = require('chai')
 const expect = chai.expect
@@ -13,16 +14,16 @@ const tests = [
       name: 'Homer Simspon',
       phoneNumber: '07123456789',
       dietaryReq: ['GLUTEN_FREE'],
-      dietaryReqOther: 'oops', // expect to be removed
+      dietaryReqOther: 'oops',
       size: 'S',
       savouryOrSweet: 'SAVOURY',
       base: 'BBQ',
-      hot: false, // expect to be removed
+      hot: false,
       howHot: 3,
       toppings: ['JALAPENOS', 'PINEAPPLE'],
-      primaryFlavour: 'WHITE_CHOC', // expect to be removed
-      secondaryFlavour: 'DARK_CHOC', // expect to be removed
-      sprinkles: false, // expect not to be removed
+      primaryFlavour: 'WHITE_CHOC',
+      secondaryFlavour: 'DARK_CHOC',
+      sprinkles: false,
       deliveryOrCollection: 'COLLECT'
     },
     output: {
@@ -40,6 +41,12 @@ const tests = [
       secondaryFlavour: null,
       sprinkles: false,
       deliveryOrCollection: 'COLLECT'
+    },
+    expectedRemoved: {
+      dietaryReqOther: null,
+      howHot: null,
+      primaryFlavour: null,
+      secondaryFlavour: null
     }
   },
   {
@@ -70,7 +77,7 @@ const tests = [
       base: 'TOMATO',
       hot: true,
       howHot: null,
-      toppings: null,
+      toppings: [],
       primaryFlavour: 'WHITE_CHOC',
       secondaryFlavour: 'DARK_CHOC',
       sprinkles: false,
@@ -253,16 +260,72 @@ const tests = [
         summary: { totalHits: 0 }
       }
     }
+  },
+  {
+    cardscript: require('./fixtures/example-empty-array.json'),
+    input: {
+      optionsCheck: null,
+      options: []
+    },
+    output: {
+      optionsCheck: null,
+      options: []
+    }
+  },
+  {
+    cardscript: require('./fixtures/example-empty-array.json'),
+    input: {
+      optionsCheck: true,
+      options: []
+    },
+    output: {
+      optionsCheck: true,
+      options: []
+    }
+  },
+  {
+    cardscript: require('./fixtures/example-empty-array.json'),
+    input: {
+      optionsCheck: false,
+      options: []
+    },
+    output: {
+      optionsCheck: false,
+      options: []
+    }
+  },
+  {
+    cardscript: require('./fixtures/example-empty-array.json'),
+    input: {
+      optionsCheck: false,
+      options: ['A']
+    },
+    output: {
+      optionsCheck: false,
+      options: []
+    }
+  },
+  {
+    cardscript: require('./fixtures/example-empty-array.json'),
+    input: {
+      optionsCheck: true,
+      options: ['A']
+    },
+    output: {
+      optionsCheck: true,
+      options: ['A']
+    }
   }
 ]
 
 describe('Run some Cardscript-post-processor conversions', function () {
   for (const [idx, { cardscript, input, output }] of tests.entries()) {
-    it(`Test ${idx + 1}`, function () {
-      const [transformedData, removed] = processor(cardscript, input)
+    it(`Test ${idx + 1}`, async function () {
+      const defaults = await extractDefaults(cardscript)
+      const [transformedData] = processor(cardscript, input, defaults)
       expect(transformedData).to.eql(output)
-      const nullValues = Object.entries(output).filter(([key, value]) => value === null).map(([key]) => key)
-      expect(nullValues.sort()).to.eql(removed.sort())
+      // const nullValues = Object.entries(output).filter(([key, value]) => value === null).map(([key]) => key)
+      // expect(nullValues.sort()).to.eql(removed.sort())
     })
   }
 })
